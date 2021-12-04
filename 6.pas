b5dbb169f4b847dcc4ddc0c0152aa251f15f2df9
alex <78720630+alex-sikach@users.noreply.github.com>
2021-12-04 22:47:51 +0200

    create 6.pas

---
+++ 6.pas
@@ -0,0 +1,10 @@
+var n1, n2, n3 : integer;
+begin
+  read(n1, n2, n3);
+  if(n1 >=1) and (n1 <= 10) then
+    writeln(n1);
+  if(n2 >=1) and (n2 <= 10) then
+    writeln(n2);
+  if(n3 >=1) and (n3 <= 10) then
+    writeln(n2);
+end.
